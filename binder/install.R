59fe5c12bf81d5ae665b764790023aaca385754d
Greg Tucker <gtucker@colorado.edu>
2020-08-27 12:56:10 -0600

    Initialize binder-R

---
+++ binder/install.R
@@ -0,0 +1,11 @@
+# Regular packages
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages("tidyverse") # for all tidyverse functionality
+
+# Bioconductor packages
+# install.packages("BiocManager")
+# BiocManager::install("package")
+
+# GitHub packages (tag = commit, branch or release tag)
+# install.packages("devtools")
+# devtools::install_github("user/rep", ref = "tag")
